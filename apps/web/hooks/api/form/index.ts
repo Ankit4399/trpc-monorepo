c272f0c3f3b078c1a2aaef0bf0058303deefbb06
Ankit4399 <ankitkumar380780@gmail.com>
2026-05-30 18:00:31 +0530

    feat: add useCreateForm hook for form creation with TRPC integration

---
+++ apps/web/hooks/api/form/index.ts
@@ -0,0 +1,32 @@
+
+import { trpc } from "~/trpc/client"
+
+export const useCreateForm = ()=>{
+    const utils = trpc.useUtils();
+    const {mutateAsync : createFormAsync,
+         mutate : createForm,
+         error,
+         failureCount,
+         isError,
+         isIdle,
+         isSuccess,
+         status
+        } 
+         = trpc.form.createForm.useMutation({
+            onSuccess : async ()=>{
+                // Invalidate form queries here if you add them later
+                await utils.form.invalidate();
+            }
+         })
+
+    return {
+        createFormAsync,
+        createForm,
+        error,
+        failureCount,
+        isError,
+        isIdle,
+        isSuccess,
+        status
+    }
+}
